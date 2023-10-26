b40e7612416462bbc0d4569c7a386852e295c6db
jaffar_aleem <jaffar.aleem@quanterian.com>
2023-10-26 14:44:42 -0700

    for production

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, send_from_directory, redirect, url_for, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS
-from flask_pymongo import PyMongo
 from bson import json_util
-app = Flask(__name__)
-CORS(app)
+import os
 from pymongo.mongo_client import MongoClient
+
+app = Flask(__name__, static_folder='build')
+CORS(app)
 uri = "mongodb+srv://jaleem:jaffar123@cluster0.aq0mtm8.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
 DB_NAME = "postappdb"
 db = client[DB_NAME]
+
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
@@ -19,25 +21,23 @@ except Exception as e:
     print(e)
 
 
-@app.route('/')
-def home():
-    return "connected"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
     
 @app.route('/post_data', methods=['POST'])
 def post_data():
-    data = request.get_json()  # Get JSON data from the request
-
-    # Access the "posts" collection
+    data = request.get_json()  
     posts_collection = db.posts
-
-    # Insert the post_data into the collection
     result = posts_collection.insert_one(data)
-
     if result.acknowledged:
         return jsonify({"message": "Post added successfully"})
     else:
         return jsonify({"message": "Failed to add post"})
-
 
 @app.route('/send_data', methods=['GET'])
 def send_data():
@@ -50,9 +50,7 @@ def send_data():
         post["_id"] = i
         newpost[i] = {'text':post["text"],'author':post["author"], 'date':post["date"]}
         i+=1
-
     return newpost
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
